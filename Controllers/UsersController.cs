f5c5a413712d3e2f2328132cb512797dceaf73bb
LynX-gh <76166621+LynX-gh@users.noreply.github.com>
2025-06-14 03:13:07 +0530

    Init: RealWorld API

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealWorldBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost(Name = "Login")]
+        public ActionResult<User> login()
+        {
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealWorldBackend
+{
+    public class User
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Token { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        public string? Bio {  get; set; }
+
+        public string? Image { get; set; }
+    }
+}
